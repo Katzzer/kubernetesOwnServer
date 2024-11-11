f90f890fc31f0fd786a1da46f2a8c405621e507b
Katzz Win11 PC <katzz@seznam.cz>
2024-11-11 22:44:27 +0100

    Update backend URL handling and improve error management
    Revised backend URL configuration to use a relative '/api' path and added proxy settings in Vite config. Improved error handling in frontend app and provided detailed access instructions in README.

--- frontend-demo-app/src/app.tsx
+++ frontend-demo-app/src/app.tsx
@@ -1,31 +1,34 @@
-import './app.css'
+import './app.css';
 import axios from "axios";
 import {useState} from "react";
 
 export function App() {
-  const [messageFromBackend, setMessageFromBackend] = useState('')
+    const [messageFromBackend, setMessageFromBackend] = useState('')
 
     function getBackendUrl() {
-        // const viteApiUrl = import.meta.env.VITE_API_URL || 'http://k8s-backend-demo-app:8080';
-        const viteApiUrl = 'backend.192.168.1.241.nip.io';
+        const viteApiUrl = '/api';
         console.log('viteApiUrl = ' + viteApiUrl)
         return viteApiUrl;
     }
 
     async function sendTestingRequestToBackend() {
-
-        const response= await axios.get(getBackendUrl());
-        console.log(response.data);
-        setMessageFromBackend(response.data);
+        try {
+            const response = await axios.get(getBackendUrl());
+            console.log(response.data);
+            setMessageFromBackend(response.data);
+        } catch (error) {
+            console.error('Error fetching data from backend:', error);
+            setMessageFromBackend('Error fetching data from backend');
+        }
     }
 
-  return (
-    <>
-        <div style={'margin-bottom: 20px'}>Backend url is: <b>{getBackendUrl()}</b></div>
-        <button onClick={sendTestingRequestToBackend}>Get data from backend</button>
-        <br/>
-        {messageFromBackend && <div style={'margin-top: 20px'}>Message from backend = {messageFromBackend}</div>}
-        <br/>
-    </>
-  )
+    return (
+        <>
+            <div style={{ marginBottom: '20px' }}>Backend url is: <b>{getBackendUrl()}</b></div>
+            <button onClick={sendTestingRequestToBackend}>Get data from backend</button>
+            <br/>
+            {messageFromBackend && <div style={{ marginTop: '20px' }}>Message from backend = {messageFromBackend}</div>}
+            <br/>
+        </>
+    )
 }
--- frontend-demo-app/vite.config.ts
+++ frontend-demo-app/vite.config.ts
@@ -4,4 +4,13 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend.192.168.1.241.nip.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
